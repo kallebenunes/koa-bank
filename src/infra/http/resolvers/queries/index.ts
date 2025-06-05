03db361b1dbd3a528e09c1252e85976531f3d90e
Kallebe Nunes <kallebe.nunes@trinto.com.br>
2025-06-04 23:45:22 -0300

    feature: create cache repository and set docs on graphql requests

--- src/infra/http/resolvers/queries/index.ts
+++ src/infra/http/resolvers/queries/index.ts
@@ -2,6 +2,7 @@ import { fetchAccounts } from './fetch-accounts';
 import { getAccountBalance } from './get-account-balance';
 
 export const QUERIES = {
+  
   getAccountBalance,
   fetchAccounts,
 }
